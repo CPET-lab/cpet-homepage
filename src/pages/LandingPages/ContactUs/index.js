a0e26314caa21d5cc6be3b55f409725595919b3e
mingi <mintim01@naver.com>
2025-08-28 21:22:05 +0900

    free download -> contact us 로 변경

--- src/pages/LandingPages/ContactUs/index.js
+++ src/pages/LandingPages/ContactUs/index.js
@@ -41,8 +41,8 @@ function ContactUs() {
           routes={routes}
           action={{
             type: "external",
-            route: "https://www.creative-tim.com/product/material-kit-react",
-            label: "free download",
+            route: "https://github.com/CPET-lab",
+            label: "CONTACT US",
             color: "info",
           }}
         />
